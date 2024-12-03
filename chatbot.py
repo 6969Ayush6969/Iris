82f7b0405844aff127118bd203518d2acca24b59
6969Ayush6969 <ayush999366@gmail.com>
2024-12-03 17:47:20 +0530

    Delete chatbot.py

--- chatbot.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCMWZyhUb4EM_n8_rvaf79Zc63FWpwngfs")
-
-def generate_reply(user_input):
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(user_input)
-    return response.text
-      
